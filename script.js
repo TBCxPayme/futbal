d708ee6a211051acc8b06f84dd5df8514e4982c7
TBCxPayme <chatforgpt13@gmail.com>
2025-10-15 15:08:23 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Позволяет изменять имена игроков по клику
 document.querySelectorAll('.player, .sub').forEach(el => {
   el.addEventListener('click', () => {
     const newName = prompt('Введите новое имя игрока:', el.textContent);
